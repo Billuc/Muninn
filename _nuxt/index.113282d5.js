4562064f6cba448ae0f27df50af767de82ec017f
Billuc <45941626+Billuc@users.noreply.github.com>
2023-09-09 16:05:41 +0000

    Deploying to gh-pages from @ Billuc/second-brain@78a896bc46deaa9d263da85aa8315df70582f0f7 🚀

---
+++ _nuxt/index.113282d5.js
@@ -0,0 +1,1 @@
+import{g as e,h as o,o as r,c as t}from"./entry.cfc6213b.js";const p=e({__name:"index",setup(n){return o().replace("/journal"),(c,s)=>(r(),t("div"))}});export{p as default};
---
+++ _nuxt/index.fdad5ca8.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.cfc6213b.js";const t={};function o(r,s){return n(),c("div",null,"Events")}const a=e(t,[["render",o]]);export{a as default};
