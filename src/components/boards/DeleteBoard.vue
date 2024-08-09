23e0492edcef4a2264b5bd95dba6e7627effc71d
Billuc <billuc@hotmail.fr>
2024-08-09 16:20:15 +0200

    feat(boards): delete also removes lists and notes

--- src/components/boards/DeleteBoard.vue
+++ src/components/boards/DeleteBoard.vue
@@ -4,23 +4,39 @@ import PageAction from "@/components/common/PageAction.vue";
 import { ref } from "vue";
 import { useService } from "@/composables/useService";
 import { useRouter } from "vue-router";
-import { Board } from "@/data/models/Board";
+import { Board, CardType } from "@/data/models/Board";
 import { BoardService } from "@/data/services/boardService";
+import { NoteService } from "@/data/services/noteService";
+import { ListService } from "@/data/services/listService";
 
 interface DeleteBoardProps {
   board: Board;
 }
 
+const boardService = useService(BoardService);
+const noteService = useService(NoteService);
+const listService = useService(ListService);
+const router = useRouter();
+
 const props = defineProps<DeleteBoardProps>();
 
 const dialogOpened = ref(false);
-const boardService = useService(BoardService);
-const router = useRouter();
-
 const removing = ref(false);
 
 const deleteBoard = async () => {
   removing.value = true;
+
+  for (let card of props.board.cards) {
+    switch (card.type) {
+      case CardType.List:
+        await listService.remove(card.id);
+        break;
+      case CardType.Note:
+        await noteService.remove(card.id);
+        break;
+    }
+  }
+
   await boardService.remove(props.board.id);
 
   setTimeout(() => {
