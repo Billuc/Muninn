65316c24d80481c9b53bf91f8a4d70c0f4d3df53
Billuc <billuc@hotmail.fr>
2024-08-09 23:48:49 +0200

    feat(boards): displaying tag in home view

--- src/components/home/HomeBoardGridElement.vue
+++ src/components/home/HomeBoardGridElement.vue
@@ -3,7 +3,6 @@ import { useService } from "@/composables/useService";
 import { useSubscription } from "@/composables/useSubscription";
 import { ID } from "@/data/models/ID";
 import { computed, ref, watchEffect } from "vue";
-import NoteGridElement from "../notes/NoteGridElement.vue";
 import { BoardTagService } from "@/data/services/boardTagService";
 import { Tag } from "@/data/models/Tag";
 import { BoardService } from "@/data/services/boardService";
@@ -15,32 +14,32 @@ interface HomeBoardGridElementProps {
 
 const props = defineProps<HomeBoardGridElementProps>();
 const boardService = useService(BoardService);
-// const noteTagService = useService(NoteTagService);
+const boardTagService = useService(BoardTagService);
 
 const boardData = await boardService.get(props.id);
 const board = ref(boardData);
 useSubscription(boardService, board);
 
-// const tagData = await noteTagService.get(note.value.tagId);
-// const noteTag = ref<Tag | null>(tagData ?? null);
-// useSubscription(noteTagService, noteTag);
+const tagData = await boardTagService.get(board.value.tagId);
+const boardTag = ref<Tag | null>(tagData ?? null);
+useSubscription(boardTagService, boardTag);
 
-// const noteAndTag = computed(() => ({
-//   ...note.value,
-//   tag: noteTag.value,
-// }));
+const boardAndTag = computed(() => ({
+  ...board.value,
+  tag: boardTag.value,
+}));
 
-// watchEffect(async () => {
-//   if (!note.value.tagId) {
-//     noteTag.value = null;
-//   }
+watchEffect(async () => {
+  if (!board.value.tagId) {
+    boardTag.value = null;
+  }
 
-//   if (note.value.tagId && note.value.tagId !== noteTag.value?.id) {
-//     noteTag.value = await noteTagService.get(note.value.tagId);
-//   }
-// });
+  if (board.value.tagId && board.value.tagId !== boardTag.value?.id) {
+    boardTag.value = await boardTagService.get(board.value.tagId);
+  }
+});
 </script>
 
 <template>
-  <BoardGridElement :board="board" v-if="board" />
+  <BoardGridElement :board="boardAndTag" v-if="board" />
 </template>
